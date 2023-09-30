e9880f37c4d4aeb4eb2391d8fe5af46468215042
unknown <akshansh2002@gmail.com>
2023-09-30 23:35:43 +0530

    completed JS of the application; CSS pending

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,91 @@
 <script>
-	export let name;
+	import { onMount } from "svelte";
+
+	let newTodoData = ""
+	let todoList = []
+	let editMode = false
+	let editId = 0
+
+	function createTodo() {
+		if (newTodoData !== "") {
+			const newTodo = {
+				id: new Date().getTime(),
+				data: newTodoData,
+				status: false
+			}
+
+			todoList.push(newTodo)
+			todoList = todoList
+			newTodoData = ""
+
+			saveData()
+		}
+	}
+
+	function updateTodo() {
+		editMode = !editMode
+
+		if (newTodoData !== "") {
+			let editableTodoIndex = todoList.findIndex((todo) => todo.id === editId)
+			
+			todoList[editableTodoIndex].data = newTodoData
+			editId = 0
+			newTodoData = ""
+
+			saveData()
+		}
+	}
+
+	function updateTodoHelper(id) {
+		editMode = !editMode
+		editId = id
+	}
+
+	function deleteTodo(id) {
+		todoList = todoList.filter((todo) => todo.id !== id)
+		saveData()
+	}
+
+	function saveData() {
+		localStorage.setItem("todoList", JSON.stringify(todoList))
+	}
+
+	function loadData() {
+		const savedData = localStorage.getItem("todoList");
+    	if (savedData) {
+        	todoList = JSON.parse(savedData);
+    	}
+	}
+
+	onMount(loadData)
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h2>TO-DO Application</h2>
+
+	<input bind:value = { newTodoData } type="text">
+
+	{ #if editMode === true }
+		<button on:click = { updateTodo }>Edit</button>
+	{ :else }
+		<button on:click = { createTodo }>Add</button>
+	{ /if }
+
+	<br>
+	<div>
+		<ul>
+			{ #each todoList as todo }
+				<li>
+					<input bind:checked = { todo.status } on:change = { saveData }  type="checkbox" name="" id="">
+					<span>{ todo.data }</span>
+					<button on:click = { deleteTodo(todo.id) }>Delete</button>
+					<button on:click = { updateTodoHelper(todo.id) }>Edit</button>
+				</li>
+			{ /each }
+		</ul>
+	</div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
